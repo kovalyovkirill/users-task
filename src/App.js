0ec196ee85d691c84437ac4cdc370bc926f61327
kirill <koval4ik2009@gmail.com>
2017-06-11 18:29:06 +0300

    add active state of link, add users logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Link, Route} from 'react-router-dom';
 import UsersPage from './containers/UsersPage';
 import UserAlbums from './containers/UserAlbums';
 import AlbumPhotos from './containers/AlbumPhotos'
+import userLogo from './user-logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
         return (
             <div className="App">
                 <div className="App-header">
-                    <Link className="main-link" to="/">Users</Link>
+                    <Link className="main-link" to="/"><img height="30px" src={userLogo} alt="Users"/> Users</Link>
                 </div>
 
                 <Route exact path="/" component={UsersPage}/>
