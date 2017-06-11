5ae155d9bf4e2cde7202fc2bb4fa8a54c6b84664
kirill <koval4ik2009@gmail.com>
2017-06-11 18:39:28 +0300

    add folder image to albums

--- src/components/AlbumLink.js
+++ src/components/AlbumLink.js
@@ -4,14 +4,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import folderImg from '../image.svg';
 
 export default function AlbumLink({album}) {
-    const albumName = album.title.replace(/ /g,"_");
+    const albumName = album.title.replace(/ /g, "_");
+    const style = {
+        paddingRight: 8,
+        height: 20
+    };
 
     return (
         <div className="user-card full-width">
             <Link to={`/${album.userId}/albums/${album.id}/${albumName}/photos`}>
                 <div className="left-padding">
+                    <img style={style} src={folderImg} alt="Folder"/>
                     <b>Title</b>: {album.title}
                 </div>
             </Link>
